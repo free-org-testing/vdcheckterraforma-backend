373e5f86440eee9ff54cb6c3ebd859d40332520b
Edge Opsx <edgeops@ust-global.com>
2023-12-12 07:55:35 +0000

    VDCHECKTERRAFORMA-00: Initial commit

---
+++ terraform/vpc/default.tf
@@ -0,0 +1,12 @@
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "my-vpc"
+  cidr = "10.0.0.0/16"
+
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+    Name = "testAbhilashvpc"
+  }
+}
